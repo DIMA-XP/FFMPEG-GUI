92f5720fc9438605a7f02d551655da9b7f8b64cf
DIMA-XP <wxp0194@gmail.com>
2026-07-12 04:06:48 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form12
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form12));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.rbOutAvi = new System.Windows.Forms.RadioButton();
+            this.rbOutMkv = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbOutOgg = new System.Windows.Forms.RadioButton();
+            this.rbOutMp4 = new System.Windows.Forms.RadioButton();
+            this.rbOutWav = new System.Windows.Forms.RadioButton();
+            this.rbOutWebm = new System.Windows.Forms.RadioButton();
+            this.rbOutMp3 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(36)))), ((int)(((byte)(87)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(80, 37);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(177, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбор файла";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(21, 326);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(293, 17);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // rbOutAvi
+            // 
+            this.rbOutAvi.AutoSize = true;
+            this.rbOutAvi.Checked = true;
+            this.rbOutAvi.ForeColor = System.Drawing.Color.White;
+            this.rbOutAvi.Location = new System.Drawing.Point(14, 19);
+            this.rbOutAvi.Name = "rbOutAvi";
+            this.rbOutAvi.Size = new System.Drawing.Size(42, 17);
+            this.rbOutAvi.TabIndex = 0;
+            this.rbOutAvi.TabStop = true;
+            this.rbOutAvi.Text = "AVI";
+            this.rbOutAvi.UseVisualStyleBackColor = true;
+            // 
+            // rbOutMkv
+            // 
+            this.rbOutMkv.AutoSize = true;
+            this.rbOutMkv.ForeColor = System.Drawing.Color.White;
+            this.rbOutMkv.Location = new System.Drawing.Point(14, 42);
+            this.rbOutMkv.Name = "rbOutMkv";
+            this.rbOutMkv.Size = new System.Drawing.Size(48, 17);
+            this.rbOutMkv.TabIndex = 4;
+            this.rbOutMkv.Text = "MKV";
+            this.rbOutMkv.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbOutOgg);
+            this.groupBox2.Controls.Add(this.rbOutAvi);
+            this.groupBox2.Controls.Add(this.rbOutMkv);
+            this.groupBox2.Controls.Add(this.rbOutMp4);
+            this.groupBox2.Controls.Add(this.rbOutWav);
+            this.groupBox2.Controls.Add(this.rbOutWebm);
+            this.groupBox2.Controls.Add(this.rbOutMp3);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox2.Location = new System.Drawing.Point(116, 97);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(114, 196);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Выход";
+            // 
+            // rbOutOgg
+            // 
+            this.rbOutOgg.AutoSize = true;
+            this.rbOutOgg.ForeColor = System.Drawing.Color.White;
+            this.rbOutOgg.Location = new System.Drawing.Point(14, 134);
+            this.rbOutOgg.Name = "rbOutOgg";
+            this.rbOutOgg.Size = new System.Drawing.Size(49, 17);
+            this.rbOutOgg.TabIndex = 7;
+            this.rbOutOgg.Text = "OGG";
+            this.rbOutOgg.UseVisualStyleBackColor = true;
+            // 
+            // rbOutMp4
+            // 
+            this.rbOutMp4.AutoSize = true;
+            this.rbOutMp4.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.rbOutMp4.ForeColor = System.Drawing.Color.White;
+            this.rbOutMp4.Location = new System.Drawing.Point(14, 65);
+            this.rbOutMp4.Name = "rbOutMp4";
+            this.rbOutMp4.Size = new System.Drawing.Size(47, 17);
+            this.rbOutMp4.TabIndex = 4;
+            this.rbOutMp4.Text = "MP4";
+            this.rbOutMp4.UseVisualStyleBackColor = true;
+            // 
+            // rbOutWav
+            // 
+            this.rbOutWav.AutoSize = true;
+            this.rbOutWav.ForeColor = System.Drawing.Color.White;
+            this.rbOutWav.Location = new System.Drawing.Point(14, 157);
+            this.rbOutWav.Name = "rbOutWav";
+            this.rbOutWav.Size = new System.Drawing.Size(50, 17);
+            this.rbOutWav.TabIndex = 8;
+            this.rbOutWav.Text = "WAV";
+            this.rbOutWav.UseVisualStyleBackColor = true;
+            // 
+            // rbOutWebm
+            // 
+            this.rbOutWebm.AutoSize = true;
+            this.rbOutWebm.ForeColor = System.Drawing.Color.White;
+            this.rbOutWebm.Location = new System.Drawing.Point(14, 88);
+            this.rbOutWebm.Name = "rbOutWebm";
+            this.rbOutWebm.Size = new System.Drawing.Size(59, 17);
+            this.rbOutWebm.TabIndex = 5;
+            this.rbOutWebm.Text = "WEBM";
+            this.rbOutWebm.UseVisualStyleBackColor = true;
+            // 
+            // rbOutMp3
+            // 
+            this.rbOutMp3.AutoSize = true;
+            this.rbOutMp3.ForeColor = System.Drawing.Color.White;
+            this.rbOutMp3.Location = new System.Drawing.Point(14, 111);
+            this.rbOutMp3.Name = "rbOutMp3";
+            this.rbOutMp3.Size = new System.Drawing.Size(47, 17);
+            this.rbOutMp3.TabIndex = 6;
+            this.rbOutMp3.Text = "MP3";
+            this.rbOutMp3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(36)))), ((int)(((byte)(87)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(60, 368);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(217, 62);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Конвертировать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FFMPEG GUI BY DIMA XP";
+            // 
+            // Form12
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(17)))), ((int)(((byte)(31)))));
+            this.ClientSize = new System.Drawing.Size(335, 444);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form12";
+            this.Text = "FFMPEG GUI BY DIMA XP V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton rbOutOgg;
+        private System.Windows.Forms.RadioButton rbOutMp3;
+        private System.Windows.Forms.RadioButton rbOutWebm;
+        private System.Windows.Forms.RadioButton rbOutWav;
+        private System.Windows.Forms.RadioButton rbOutMp4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbOutMkv;
+        private System.Windows.Forms.RadioButton rbOutAvi;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form12 : Form
+    {
+        // 1. Глобальные переменные (видны всем методам)
+        string selectedFile = ""; 
+        double totalSeconds = 0; // ДОБАВЬ ЭТУ СТРОКУ
+
+        public Form12()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            // Фильтр, чтобы видеть только видеофайлы и аудиофайлы
+            open.Filter = "Видео и аудио файлы|*.mp4;*.avi;*.mkv;*.mp3;*.ogg;*.wav;*.webm;*.mov|Все файлы|*.*";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                selectedFile = open.FileName;
+                System.Media.SystemSounds.Exclamation.Play();
+                MessageBox.Show("Файл выбран: " + Path.GetFileName(selectedFile));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            // проверяем наличия API
+            if (!System.IO.File.Exists("ffmpeg.exe"))
+            {
+                MessageBox.Show("Положите файл ffmpeg.exe в папку с программой!", "Ошибка"); return;
+            } 
+
+            if (string.IsNullOrEmpty(selectedFile)) return;
+
+            // 1. Узнаем длительность видео перед стартом (хитрый трюк)
+            Process getDuration = new Process();
+            getDuration.StartInfo.FileName = "ffmpeg.exe";
+            getDuration.StartInfo.Arguments = string.Format("-i \"{0}\"", selectedFile);
+            getDuration.StartInfo.UseShellExecute = false;
+            getDuration.StartInfo.RedirectStandardError = true;
+            getDuration.StartInfo.CreateNoWindow = true;
+            getDuration.Start();
+
+            string info = getDuration.StandardError.ReadToEnd();
+            // Ищем строку Duration: 00:00:00.00
+            if (info.Contains("Duration: "))
+            {
+                string dur = info.Substring(info.IndexOf("Duration: ") + 10, 11);
+                totalSeconds = TimeSpan.Parse(dur).TotalSeconds;
+            }
+
+            progressBar1.Value = 0;
+            button2.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Определяем формат и параметры
+            string ext = ".mp4";
+            string extraArgs = "";
+
+            if (rbOutMp4.Checked)
+            {
+                ext = ".mp4";
+                // Явно указываем видеокодек libx264 и аудиокодек aac
+                // "pix_fmt yuv420p" нужен, чтобы видео открывалось стандартным плеером Windows
+                extraArgs = "-c:v libx264 -pix_fmt yuv420p -c:a aac -strict experimental";
+            }
+            if (rbOutAvi.Checked) ext = ".avi";
+            if (rbOutMkv.Checked) ext = ".mkv";
+            if (rbOutWebm.Checked) { ext = ".webm"; extraArgs = "-c:v libvpx -c:a libvorbis"; }
+            if (rbOutMp3.Checked) { ext = ".mp3"; extraArgs = "-vn -ab 192k"; } // -vn убирает видео
+            if (rbOutOgg.Checked) { ext = ".ogg"; extraArgs = "-vn -acodec libvorbis"; }
+            if (rbOutWav.Checked) { ext = ".wav"; extraArgs = "-vn"; }
+
+            string resultFile = Path.ChangeExtension(selectedFile, ext);
+
+            Process ffmpeg = new Process();
+            ffmpeg.StartInfo.FileName = "ffmpeg.exe";
+            // -y разрешает перезапись файла
+            ffmpeg.StartInfo.Arguments = string.Format("-i \"{0}\" {1} \"{2}\" -y", selectedFile, extraArgs, resultFile);
+
+            ffmpeg.StartInfo.UseShellExecute = false;
+            ffmpeg.StartInfo.CreateNoWindow = true;
+            ffmpeg.StartInfo.RedirectStandardError = true;
+            ffmpeg.Start();
+
+            using (StreamReader reader = ffmpeg.StandardError)
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Ищем в строке time=00:00:00.00
+                    if (line.Contains("time=") && totalSeconds > 0)
+                    {
+                        try
+                        {
+                            string timePart = line.Substring(line.IndexOf("time=") + 5, 11);
+                            double currentSeconds = TimeSpan.Parse(timePart).TotalSeconds;
+                            int percent = (int)((currentSeconds / totalSeconds) * 100);
+
+                            if (percent >= 0 && percent <= 100)
+                                backgroundWorker1.ReportProgress(percent);
+                        }
+                        catch { /* игнорим ошибки парсинга */ }
+                    }
+                }
+            }
+            ffmpeg.WaitForExit();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Теперь здесь реальные проценты!
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = 100;
+            button2.Enabled = true;
+            MessageBox.Show("Готово!");
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted_1(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Возвращаем кнопку в рабочее состояние
+            button2.Enabled = true;
+
+            // Заполняем прогресс-бар до конца для красоты
+            progressBar1.Value = 100;
+
+            if (e.Error != null)
+            {
+                System.Media.SystemSounds.Hand.Play(); // Звук критической ошибки (крестик)
+                MessageBox.Show("Ошибка!");
+
+            }
+            else
+            {
+                System.Media.SystemSounds.Asterisk.Play(); // Приятный звук уведомления
+                MessageBox.Show("Конвертация успешно завершена!");
+
+            }
+        }
+    }
+}
